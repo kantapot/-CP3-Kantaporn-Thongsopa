1c6b5de7c97a0fd23e9ffff9ded85f4b2419e3f9
kantapot <kantapot@scg.com>
2024-09-15 16:12:11 +0700

    Create Exercise5_1_Kantaporn_T.py

---
+++ Exercise5_1_Kantaporn_T.py
@@ -0,0 +1,12 @@
+n1 = int(input("1st number: "))
+n2 = int(input("2nd number: "))
+
+pl = n1 + n2
+mi = n1 - n2
+mu = n1 * n2
+di = n1 / n2
+
+print(n1, "+", n2, "=", pl)
+print(n1, "-", n2, "=", mi)
+print(n1, "*", n2, "=", mu)
+print(n1, "/", n2, "=", di)
